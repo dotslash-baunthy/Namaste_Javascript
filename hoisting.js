fa2b1be65b11e37fd5d9f674fe10c1b16efc4344
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2022-08-28 11:05:16 +0530

    Update hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -1,7 +1,14 @@
-console.log(myFunc);
+// console.log(myFunc);
 
 var myFunc = () => {
     console.log("Hello. My name is Akshit!");
+    var a = 1000;
 }
 
-console.log(myFunc);
+// console.log(myFunc);
+myFunc();
+
+// console.log(a);
+const c = 1000;
+var v = 200;
+let l = 5;
